0cd7448cabb32c16ad5a03f57427fa97158835a3
PRAKASH RANJAN <aryaprakashranjan@gmail.com>
2021-09-07 17:53:37 +0530

    Initial commit

---
+++ app/src/main/java/com/ranzan/viewpagerwe/FirstFragment.java
@@ -0,0 +1,47 @@
+package com.ranzan.viewpagerwe;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    private EditText etName;
+    private Button btnSend;
+    private MainActivity mainActivity;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mainActivity =(MainActivity) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etName = view.findViewById(R.id.etName);
+        btnSend = view.findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                mainActivity.setData(name);
+            }
+        });
+
+    }
+}
